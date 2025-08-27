5ef4d35610e5301696baeb95f832bd0349ecc0a1
Peloro <plfpedro@usp.br>
2025-08-26 22:53:21 -0300

    ADD: Improved instructions; New exercise added

---
+++ Exercicios/meio.py
@@ -0,0 +1,6 @@
+# Faça um programa para ler n números inteiros e armazene numa lista, retorne o valor do meio dessa lista
+# Se a lista tiver tamanho par, retorne a média dos dois valores do meio
+# Exemplo: [1, 2, 3, 4, 5] -> 3
+# Exemplo: [1, 2, 3, 4] -> (2 + 3) / 2 = 2.5
+
+numeros = []
--- Exercicios/notas.py
+++ Exercicios/notas.py
@@ -1,3 +1,6 @@
 # Faça um programa para ler a nota da prova de n alunos e armazene numa lista, calcule e imprima a media geral
+# Depois, imprima a quantidade de alunos com nota acima da média 5
+# Exemplo: [7, 8, 9, 6, 5] -> (7 + 8 + 9 + 6 + 5) / 5 = 7.0 -> 4 alunos acima da média
+# -> == próxima linha
 
 notas = []
